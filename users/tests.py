2bcf1b93714971fd0a224114f8716dfe9e95e35a
baha_2002 <baktiarzaksylykov22@gmail.com>
2024-01-29 14:00:15 +0600

    Test finished

--- users/tests.py
+++ users/tests.py
@@ -2,8 +2,15 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.test import TestCase
 
 from users.models import User
+from users.serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    UserProfileSerializer,
+    VerifyEmailSerializer
+)
 
 
 class UserApiTests(APITestCase):
@@ -56,3 +63,48 @@ class UserApiTests(APITestCase):
     #     }
     #     response = self.client.put(reverse('update_profile'), data, **self.bearer_token)
         # self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestUserSerializer(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            email='admin@test.com',
+            password='password',
+            is_active=True,
+            is_verified=True,
+            confirmation_code=4567
+        )
+
+    def test_register(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Kuper',
+            'email': 'admin2@test.com',
+            'password': 'password'
+        }
+        serializer = RegisterSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_login(self):
+        data = {
+            'email': 'admin@test.com',
+            'password': 'password'
+        }
+        serializer = LoginSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_verify_email(self):
+        data = {
+            'email': 'admin@test.com',
+            'code': 4567
+        }
+        serializer = VerifyEmailSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_profile(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Kuper'
+        }
+        serializer = UserProfileSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
